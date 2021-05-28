954bb2db0416957dca9221d630d2ff26b547420e
ᏗᎮ๏ɭɭ๏ <72794063+PmsHere@users.noreply.github.com>
2021-05-28 17:19:24 +0530

    Update song.py

--- plugins/song.py
+++ plugins/song.py
@@ -64,7 +64,7 @@ def a(client, message):
             #     m.edit("Exceeded 30mins cap")
             #     return
 
-            performer = f"[ꜱᴀꜰᴏɴᴇ ᴍᴜꜱɪᴄ]" 
+            performer = f"[സിനിമ കമ്പനി | Cinema Company [MT] ™]" 
             thumb_name = f'thumb{message.message_id}.jpg'
             thumb = requests.get(thumbnail, allow_redirects=True)
             open(thumb_name, 'wb').write(thumb.content)
@@ -94,7 +94,7 @@ def a(client, message):
         m.delete()
         message.delete()
     except Exception as e:
-        m.edit('**An Error Occured. Please Report This To @SafoTheBot !!**')
+        m.edit('**An Error Occured. Please Report This To @Fex_On !!**')
         print(e)
     try:
         os.remove(audio_file)
